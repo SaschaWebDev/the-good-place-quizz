28a487fa6df76c78201456e618ee6d78f86f1acd
Sascha Majewsky <sascha.majewsky@pm.me>
2020-02-23 17:38:02 +0100

    Added welcome view

--- the-good-place-quizz/src/App.js
+++ the-good-place-quizz/src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          React Quizz
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <p>React Quizz</p>
       </header>
     </div>
   );
---
+++ the-good-place-quizz/src/views/Welcome.js
@@ -0,0 +1,18 @@
+import '../Welcome.css';
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div class='welcome-container'>
+        <div>WELCOME</div>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
