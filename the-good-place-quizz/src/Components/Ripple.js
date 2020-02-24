13418a6ab379c4a8fffcdbb9534688012d07f39e
Sascha Majewsky <sascha.majewsky@pm.me>
2020-02-24 13:21:54 +0100

    Added a ripple effect to button onClick

---
+++ the-good-place-quizz/src/Components/Ripple.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+import '../styles/Ripple.css';
+
+const styleRipple = {
+  position: 'relative',
+  overflow: 'hidden',
+};
+
+const styleRippleContainer = {
+  position: 'absolute',
+  top: '0',
+  right: '0',
+  bottom: '0',
+  left: '0',
+};
+
+const styleSpan = {
+  transform: 'scale(0)',
+  borderRadius: '100%',
+  position: 'absolute',
+  opacity: '0.75',
+  backgroundColor: '#ffffff',
+  animation: 'ripple 850ms',
+};
+
+class Ripple extends React.Component {
+  initializeState = () => {
+    return {
+      spanStyles: {},
+      count: 0,
+    };
+  };
+  state = this.initializeState();
+
+  /* Debounce Code to call the Ripple removing function */
+  callCleanUp = (cleanup, delay) => {
+    return function() {
+      clearTimeout(2);
+      setTimeout(() => {
+        cleanup();
+      }, delay);
+    };
+  };
+
+  showRipple = e => {
+    const rippleContainer = e.currentTarget;
+    const size = rippleContainer.offsetWidth;
+    const pos = rippleContainer.getBoundingClientRect();
+    const x = e.pageX - pos.x - size / 2;
+    const y = e.pageY - pos.y - size / 2;
+
+    const spanStyles = {
+      top: y + 'px',
+      left: x + 'px',
+      height: size + 'px',
+      width: size + 'px',
+    };
+
+    const count = this.state.count + 1;
+    this.setState({
+      spanStyles: { ...this.state.spanStyles, [count]: spanStyles },
+      count: count,
+    });
+  };
+
+  cleanUp = () => {
+    const initialState = this.initializeState();
+    this.setState({ ...initialState });
+  };
+
+  renderRippleSpan = () => {
+    const { showRipple = false, spanStyles = {} } = this.state;
+    const spanArray = Object.keys(spanStyles);
+    if (spanArray && spanArray.length > 0) {
+      return spanArray.map((key, index) => {
+        return (
+          <span
+            key={'spanCount_' + index}
+            className=''
+            style={{ ...spanStyles[key] }}></span>
+        );
+      });
+    } else {
+      return null;
+    }
+  };
+
+  render() {
+    const { children = null, classes = '' } = this.props;
+    return (
+      <div
+        onClick={this.props.onClick}
+        ref='targetElement'
+        className={'ripple answer ' + classes}>
+        {children}
+        <div
+          className='rippleContainer'
+          onMouseDown={this.showRipple}
+          onMouseUp={this.callCleanUp(this.cleanUp, 2000)}>
+          {this.renderRippleSpan()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Ripple;
--- the-good-place-quizz/src/views/Quiz.js
+++ the-good-place-quizz/src/views/Quiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Chevron from '../styles/chevron-left.png';
+import Ripple from '../Components/Ripple.js';
 import '../styles/Quiz.css';
 
 class Quiz extends React.Component {
@@ -68,6 +69,7 @@ class Quiz extends React.Component {
   };
 
   nextQuestion = () => {
+    console.log('TRIGGERED');
     if (this.state.questionCount === 1) {
       this.setState({
         questionCount: 2,
@@ -105,21 +107,21 @@ class Quiz extends React.Component {
 
   render() {
     return (
-      <div class='quiz-container noselect'>
-        <div class='upper-container'>
-          <div class='upper-info-container'>
-            <div class='back-button' onClick={() => this.goBack()}>
+      <div className='quiz-container noselect'>
+        <div className='upper-container'>
+          <div className='upper-info-container'>
+            <div className='back-button' onClick={() => this.goBack()}>
               <img
-                class='cta-svg noselect'
+                className='cta-svg noselect'
                 src={Chevron}
                 alt='Icon of a chevron pointing left.'
               />
             </div>
-            <div class='quiz-counter'>
-              <div class='quiz-counter-current'>
+            <div className='quiz-counter'>
+              <div className='quiz-counter-current'>
                 0{this.state.questionCount}
               </div>
-              <div class='quiz-counter-remaining'>/ 04</div>
+              <div className='quiz-counter-remaining'>/ 04</div>
             </div>
           </div>
           <div className='progress-bar'>
@@ -128,29 +130,36 @@ class Quiz extends React.Component {
               style={{ width: `${this.state.percentage}%` }}
             />
           </div>
-          <div class='question-text'>{this.state.question}</div>
+          <div className='question-text'>{this.state.question}</div>
         </div>
 
-        <div class='lower-container'>
-          <div class='middle-container'></div>
-          <div class='middle-second-container'></div>
-          <div class='middle-third-container'></div>
+        <div className='lower-container'>
+          <div className='middle-container'></div>
+          <div className='middle-second-container'></div>
+          <div className='middle-third-container'></div>
 
-          <div class='answer-container'>
-            <div class='answer answer-one' onClick={() => this.nextQuestion()}>
-              <div class='answer-symbol'>a</div>
-              <div class='answer-text'>{this.state.answerOne}</div>
-            </div>
-            <div class='answer answer-two' onClick={() => this.nextQuestion()}>
-              <div class='answer-symbol'>b</div>
-              <div class='answer-text'>{this.state.answerTwo}</div>
-            </div>
-            <div
-              class='answer answer-three'
+          <div className='answer-container'>
+            <Ripple
+              className='answer answer-one btn'
               onClick={() => this.nextQuestion()}>
-              <div class='answer-symbol'>c</div>
-              <div class='answer-text'>{this.state.answerThree}</div>
-            </div>
+              <span className='ion-help-circled'></span>
+              <div className='answer-symbol'>a</div>
+              <div className='answer-text'>{this.state.answerOne}</div>
+            </Ripple>
+            <Ripple
+              className='answer answer-two btn'
+              onClick={() => this.nextQuestion()}>
+              <span className='ion-help-circled'></span>
+              <div className='answer-symbol'>b</div>
+              <div className='answer-text'>{this.state.answerTwo}</div>
+            </Ripple>
+            <Ripple
+              className='answer answer-three btn'
+              onClick={() => this.nextQuestion()}>
+              <span className='ion-help-circled'></span>
+              <div className='answer-symbol'>c</div>
+              <div className='answer-text'>{this.state.answerThree}</div>
+            </Ripple>
           </div>
         </div>
       </div>
