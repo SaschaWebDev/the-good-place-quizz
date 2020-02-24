75a1a0f76ac226fdbfed0c6b11aec9c7c07ce5d0
Sascha Majewsky <sascha.majewsky@pm.me>
2020-02-24 06:43:52 +0100

    Refactored quizz question and answers

--- the-good-place-quizz/src/views/Quiz.js
+++ the-good-place-quizz/src/views/Quiz.js
@@ -9,23 +9,97 @@ class Quiz extends React.Component {
     this.state = {
       percentage: 25,
       questionCount: 1,
-      questionText:
-        'Deine Oma hat Geburtstag und du möchtest ihr Blumen schenken',
-      questionOne:
-        'Deine Oma hat Geburtstag und du möchtest ihr Blumen schenken',
-      questionTwo: 'Du bist durstig und kaufst dir Milch',
-      questionThree: 'Du verreist in den Urlaub nach Ungarn',
+      question: 'Deine Oma hat Geburtstag und du möchtest ihr Blumen schenken',
       questionFour: 'Du möchtest deine Stromrechnung senken',
+      answerOne: 'Auslandimport günstiger Blumen',
+      answerTwo: 'Blumenkauf im lokalen Blumenladen',
+      answerThree: 'Blumen selber pflücken',
     };
   }
 
   goBack = () => {
     if (this.state.questionCount === 1) {
       this.setState({
-        percentage: 25,
-        questionText: this.state.questionOne,
+        questionCount: 1,
+        percentage: 0,
+        question:
+          'Deine Oma hat Geburtstag und du möchtest ihr Blumen schenken',
+        answerOne: 'Auslandimport günstiger Blumen',
+        answerTwo: 'Blumenkauf im lokalen Blumenladen',
+        answerThree: 'Blumen selber pflücken',
       });
       this.props.history.push('/');
+    }
+
+    if (this.state.questionCount === 2) {
+      this.setState({
+        questionCount: 1,
+        percentage: 25,
+        question:
+          'Deine Oma hat Geburtstag und du möchtest ihr Blumen schenken',
+        answerOne: 'Auslandimport günstiger Blumen',
+        answerTwo: 'Blumenkauf im lokalen Blumenladen',
+        answerThree: 'Blumen selber pflücken',
+      });
+    }
+
+    if (this.state.questionCount === 3) {
+      this.setState({
+        questionCount: 2,
+        percentage: 50,
+        question:
+          'Für dein Lieblings- rezept benötigst du noch etwas frische Milch',
+        answerOne: 'Frischmilchkauf beim regionalen Bauern',
+        answerTwo: 'Kauf von H-Milch im Supermarkt',
+        answerThree: 'Kauf von veganer Mandelmilch',
+      });
+    }
+
+    if (this.state.questionCount === 4) {
+      this.setState({
+        questionCount: 3,
+        percentage: 75,
+        question: 'Du planst für deine nächste Reise einen Trip nach Ungarn',
+        answerOne: 'Suche nach günstigen Flug bei Billig-Airline',
+        answerTwo: 'Mieten von bezahlbaren Auto bei Sixt',
+        answerThree: 'Erwerb eines Bahnticket bei DB',
+      });
+    }
+  };
+
+  nextQuestion = () => {
+    if (this.state.questionCount === 1) {
+      this.setState({
+        questionCount: 2,
+        percentage: 50,
+        question:
+          'Für dein Lieblings- rezept benötigst du noch etwas frische Milch',
+        answerOne: 'Frischmilchkauf beim regionalen Bauern',
+        answerTwo: 'Kauf von H-Milch im Supermarkt',
+        answerThree: 'Kauf von veganer Mandelmilch',
+      });
+    }
+
+    if (this.state.questionCount === 2) {
+      this.setState({
+        questionCount: 3,
+        percentage: 75,
+        question: 'Du planst für deine nächste Reise einen Trip nach Ungarn',
+        answerOne: 'Suche nach günstigen Flug bei Billig-Airline',
+        answerTwo: 'Mieten von bezahlbaren Auto bei Sixt',
+        answerThree: 'Erwerb eines Bahnticket bei DB',
+      });
+    }
+
+    if (this.state.questionCount === 3) {
+      this.setState({
+        questionCount: 4,
+        percentage: 100,
+        question: 'Du versuchst deine Strom- rechnung zu reduzieren',
+        answerOne: 'Vertrag mit günstigen Atomstromanbieter',
+        answerTwo: 'Solaranlagen am Dach anbringen',
+        answerThree: 'Vertrag mit Stromanbieter erneuerbarer Energien',
+      });
     }
   };
 
@@ -54,14 +128,30 @@ class Quiz extends React.Component {
               style={{ width: `${this.state.percentage}%` }}
             />
           </div>
-          <div class='question-text'>{this.state.questionText}</div>
+          <div class='question-text'>{this.state.question}</div>
         </div>
 
         <div class='lower-container'>
           <div class='middle-container'></div>
           <div class='middle-second-container'></div>
           <div class='middle-third-container'></div>
-          TEST
+
+          <div class='answer-container'>
+            <div class='answer answer-one' onClick={() => this.nextQuestion()}>
+              <div class='answer-symbol'>a</div>
+              <div class='answer-text'>{this.state.answerOne}</div>
+            </div>
+            <div class='answer answer-two' onClick={() => this.nextQuestion()}>
+              <div class='answer-symbol'>b</div>
+              <div class='answer-text'>{this.state.answerTwo}</div>
+            </div>
+            <div
+              class='answer answer-three'
+              onClick={() => this.nextQuestion()}>
+              <div class='answer-symbol'>c</div>
+              <div class='answer-text'>{this.state.answerThree}</div>
+            </div>
+          </div>
         </div>
       </div>
     );
---
+++ the-good-place-quizz/src/views/Result.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import '../styles/Result.css';
+
+class Result extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  /*  redirectToQuiz = () => {
+    this.props.history.push('/quiz');
+  }; */
+
+  render() {
+    return (
+      <div class='result-container'>
+        {/* <div class='title noselect'>The Good Place</div>
+        <div class='title-sub noselect'>
+          Hast du einen Platz im Good Place verdient? Beantworte einfach die
+          Fragen intuitiv.
+        </div>
+        <img
+          class='cta-svg noselect'
+          src={welcomeSVG}
+          alt='Illustraiton of a sitting woman with a laptop in red.'
+        />
+        <button
+          class='cta-button noselect'
+          onClick={() => this.redirectToQuiz()}>
+          Teste dich!
+        </button> */}
+      </div>
+    );
+  }
+}
+
+export default Result;
