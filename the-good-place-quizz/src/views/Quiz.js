43a694e9d884999f0db86b17aa4b09eeb356859c
Sascha Majewsky <sascha.majewsky@pm.me>
2020-02-24 04:59:12 +0100

    Created further UI and a back button for the quiz view

--- the-good-place-quizz/src/views/Quiz.js
+++ the-good-place-quizz/src/views/Quiz.js
@@ -1,18 +1,68 @@
 import React from 'react';
 
+import Chevron from '../styles/chevron-left.png';
 import '../styles/Quiz.css';
 
 class Quiz extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      percentage: 25,
+      questionCount: 1,
+      questionText:
+        'Deine Oma hat Geburtstag und du möchtest ihr Blumen schenken',
+      questionOne:
+        'Deine Oma hat Geburtstag und du möchtest ihr Blumen schenken',
+      questionTwo: 'Du bist durstig und kaufst dir Milch',
+      questionThree: 'Du verreist in den Urlaub nach Ungarn',
+      questionFour: 'Du möchtest deine Stromrechnung senken',
+    };
   }
+
+  goBack = () => {
+    if (this.state.questionCount === 1) {
+      this.setState({
+        percentage: 25,
+        questionText: this.state.questionOne,
+      });
+      this.props.history.push('/');
+    }
+  };
 
   render() {
     return (
-      <div class='quiz-container'>
-        <div class='upper-container'>TEST</div>
-        <div class='lower-container'>TEST</div>
+      <div class='quiz-container noselect'>
+        <div class='upper-container'>
+          <div class='upper-info-container'>
+            <div class='back-button' onClick={() => this.goBack()}>
+              <img
+                class='cta-svg noselect'
+                src={Chevron}
+                alt='Icon of a chevron pointing left.'
+              />
+            </div>
+            <div class='quiz-counter'>
+              <div class='quiz-counter-current'>
+                0{this.state.questionCount}
+              </div>
+              <div class='quiz-counter-remaining'>/ 04</div>
+            </div>
+          </div>
+          <div className='progress-bar'>
+            <div
+              className='filler'
+              style={{ width: `${this.state.percentage}%` }}
+            />
+          </div>
+          <div class='question-text'>{this.state.questionText}</div>
+        </div>
+
+        <div class='lower-container'>
+          <div class='middle-container'></div>
+          <div class='middle-second-container'></div>
+          <div class='middle-third-container'></div>
+          TEST
+        </div>
       </div>
     );
   }
